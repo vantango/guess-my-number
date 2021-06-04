fd8e3b43d9aca7a2ade606c07e1df580c4aae6b6
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-06-03 19:58:38 -0700

    added low/high guess and score logic and added style change logic when user wins

--- script.js
+++ script.js
@@ -9,16 +9,47 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //   When there is no input
   if (!guess) {
     console.log(
-      (document.querySelector('.message').textContent = '🚨 No number!')
+      (document.querySelector('.message').textContent =
+        '⛔ Not a valid number!')
     );
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😓 You lost...';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😓 You lost...';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
