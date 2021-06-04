19385188c4ef10c6cf695e9ac0ed599f1c9f8bef
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-06-03 17:27:16 -0700

    added error handling for numbers outside of 1-20 and created function for secret number randomization

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent =  13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    console.log(
+      (document.querySelector('.message').textContent = '🚨 No number!')
+    );
+  }
+});
